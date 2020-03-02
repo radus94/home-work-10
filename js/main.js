07cda6986b3f00ef895e7035985dcdf19eadc56c
unknown <radusirghev@gmail.com>
2020-03-02 15:21:50 +0200

    Add typeof

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@ else {
 
 var result = ( myCurrentAge >= 18 )?"<div>Method Ternary Operator: <span class='red'>I am over 25 years old</span></div>" : "<div>Method Ternary Operator: <span class='green'>I am under 25</span></div>";
  document.write(result)
+
+
+
+document.write("<div>Type: " + typeof 5 + "<div>")
+
+document.write("<div>Type: " + typeof "String" + "<div>")
+
+document.write("<div>Type: " + typeof true + "<div>")
+
+document.write("<div>Type: " + typeof x + "<div>")
+
+document.write("<div>Type: " + typeof {a: 1} + "<div>")
+
+document.write("<div>Type: " + typeof function() {} + "<div>")
+
